c277e8d0b0d62c19ab00b3175d46348389e26052
web-collab <tanmoy587@gmail.com>
2024-07-28 07:45:51 +0530

    Project Setup

---
+++ src/main/java/org/employee/employee_management_system/EmployeeManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package org.employee.employee_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("\nHI");
+		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+	}
+
+}
