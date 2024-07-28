a5820c575b0b087f0c95e75adc4d3af4d6c573c4
web-collab <tanmoy587@gmail.com>
2024-07-28 18:35:09 +0530

    3.@GetMapping() @PostMapping() REST API, Service Layer Calling

--- src/main/java/org/employee/employee_management_system/EmpController.java
+++ src/main/java/org/employee/employee_management_system/EmpController.java
@@ -4,19 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController
 public class EmpController {
+
+    List<Employee> employees = new ArrayList<>();
+
     @GetMapping("employees")
     public List<Employee> getAllEmployees() {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(null);
-        Employee emp = new Employee();
-        emp.setName("Shivam");
-        employees.add(emp);
         return employees;
+    }
+    
+    @PostMapping("employees")
+    public String createEmployee(@RequestBody Employee employee) {
+        employees.add(employee);
+        return "Saved Successfully";
     }
     
 }
--- src/main/java/org/employee/employee_management_system/Employee.java
+++ src/main/java/org/employee/employee_management_system/Employee.java
@@ -1,24 +1,10 @@
 package org.employee.employee_management_system;
 
+import lombok.Data;
+
+@Data
 public class Employee {
     private String name;
     private String phone;
     private String email;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    
 }
