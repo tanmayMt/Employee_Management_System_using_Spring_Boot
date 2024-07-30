ee8800386920c39817b7580fa0e37bc9e38bf485
web-collab <tanmoy587@gmail.com>
2024-07-30 07:05:17 +0530

    4.getAllEmployees(), createEmployee() with EmployeeServiceImpl

--- src/main/java/org/employee/employee_management_system/EmpController.java
+++ src/main/java/org/employee/employee_management_system/EmpController.java
@@ -2,7 +2,9 @@ package org.employee.employee_management_system;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,12 @@ public class EmpController {
         //employees.add(employee);
         return employeeService.createEmployee(employee);
         //return "Saved Successfully";
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        if (employeeService.deleteEmployee(id))
+            return "Deleted Succesfully";
+        return "Not Found";
     }
 }
--- src/main/java/org/employee/employee_management_system/EmployeeManagementSystemApplication.java
+++ src/main/java/org/employee/employee_management_system/EmployeeManagementSystemApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementSystemApplication {
 
 	public static void main(String[] args) {
-		//System.out.println("\nHI");
+		// System.out.println("\nHI");
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
-
 }
